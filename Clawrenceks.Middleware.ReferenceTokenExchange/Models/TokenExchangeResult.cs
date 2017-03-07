d078d7161e9717be983283b32e857567c957c88d
Chris Lawrence <chris@sect0r.co.uk>
2017-03-07 22:06:21 +0000

    Switch to internal setters on TokenExchangeResult

--- Clawrenceks.Middleware.ReferenceTokenExchange/Models/TokenExchangeResult.cs
+++ Clawrenceks.Middleware.ReferenceTokenExchange/Models/TokenExchangeResult.cs
@@ -4,8 +4,8 @@ namespace Clawrenceks.ReferenceTokenExchange.Models
 {
     public class TokenExchangeResult
     {
-        public string ReferenceToken { get; set; }
-        public string AccessToken { get; set; }
-        public DateTimeOffset AccessTokenExpiryTime { get; set; }
+        public string ReferenceToken { get; internal set; }
+        public string AccessToken { get; internal set; }
+        public DateTimeOffset AccessTokenExpiryTime { get; internal set; }
     }
 }
