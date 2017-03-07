8f0f37edc1eb9b0d4a86f7a6b11316ae3648699d
Chris Lawrence <chris@sect0r.co.uk>
2017-03-07 22:10:31 +0000

    Change the add caching extension method to AddCaching

--- Clawrenceks.Middleware.ReferenceTokenExchange/Configuration/DependencyInjection/ReferenceTokenExchangeBuilderExtensions.cs
+++ Clawrenceks.Middleware.ReferenceTokenExchange/Configuration/DependencyInjection/ReferenceTokenExchangeBuilderExtensions.cs
@@ -7,13 +7,13 @@ namespace Clawrenceks.ReferenceTokenExchange.Configuration
 {
     public static class ReferenceTokenExchangeBuilderExtensions
     {
-        public static IReferenceTokenExchangeBuilder AddInMemoryCache(this IReferenceTokenExchangeBuilder builder)
+        public static IReferenceTokenExchangeBuilder AddCaching(this IReferenceTokenExchangeBuilder builder)
         {
-            AddInMemoryCache<InMemoryTokenExchangeCache>(builder, ServiceLifetime.Singleton);
+            AddCaching<InMemoryTokenExchangeCache>(builder, ServiceLifetime.Singleton);
             return builder;
         }
 
-        public static IReferenceTokenExchangeBuilder AddInMemoryCache<T>(this IReferenceTokenExchangeBuilder builder, ServiceLifetime lifetime )
+        public static IReferenceTokenExchangeBuilder AddCaching<T>(this IReferenceTokenExchangeBuilder builder, ServiceLifetime lifetime )
             where T: class, ITokenExchangeCache
         {
             builder.Services.AddTransient<ICachingTokenExchangeService, TokenExchangeServiceWithCache>();
